873d1455dbd57cdff9a33740f51c89864270285d
eduardo yco jr <eduardo.ycojr@gmail.com>
2020-05-30 11:30:44 +0800

    Initial Commit

---
+++ fschhashnotif/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fschhashnotif
+//
+//  Created by Eduardo Yco Jr on 5/30/20.
+//  Copyright © 2020 Eduardo Yco Jr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
